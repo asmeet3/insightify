05fb5520db7379b9957ff66c3c587ca23163f5bb
Asmeet Singh <asmeet003@gmail.com>
2024-03-19 22:13:42 +0530

    initial

---
+++ flask-server/server.py
@@ -0,0 +1,68 @@
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
+def get_pdf_text(pdf_path):
+    with open(pdf_path, 'rb') as file:
+        pdf_reader = PdfReader(file)
+        text = ''
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+def handle_userinput(user_question, conversation_chain):
+    response = conversation_chain.invoke({'question': user_question})
+    chat_history = response['chat_history']
+    for i, message in enumerate(chat_history):
+        if i % 2 == 0:
+            print("User:", message.content)
+        else:
+            print("Bot:", message.content)
+
+def main():
+    load_dotenv()
+
+    pdf_path = input("Enter the path to your PDF: ")
+    raw_text = get_pdf_text(pdf_path)
+    text_chunks = get_text_chunks(raw_text)
+    vectorstore = get_vectorstore(text_chunks)
+    conversation_chain = get_conversation_chain(vectorstore)
+
+    print("Welcome to the chat with multiple PDFs!")
+    while True:
+        user_question = input("Ask a question about your documents (type 'exit' to quit): ")
+        if user_question.lower() == 'exit':
+            break
+        handle_userinput(user_question, conversation_chain)
+
+if __name__ == '__main__':
+    main()
